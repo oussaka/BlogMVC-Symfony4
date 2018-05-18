7a3174e204aa31d6fb0e1ede17535ff1f28342d9
oussaka <oussaka@users.noreply.github.com>
2018-05-18 07:12:48 +0000

    autowire AuthenticationUtils

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -10,8 +13,8 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="login")
      **/
-    public function login () {
-        $authenticationUtils = $this->get('security.authentication_utils');
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
@@ -19,7 +22,7 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', array(
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error,
         ));
     }
 

c189e3640ac906ae2a084b143bb5dbd768b9afd9
oussaka <oussaka@users.noreply.github.com>
2018-05-18 17:01:44 +0000

    unit test fix

--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -3,23 +3,33 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 
 class PostControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+
+    public function testLoginPage()
     {
-        // Create a new client to browse the application
         $client = static::createClient();
 
+        $crawler = $client->request('GET', '/login');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /* public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = self::createClient();
+
         // Go to the list view
-        $crawler = $client->request('GET', '/post/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /post/");
+        $crawler = $client->request('GET', $url);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /posts");
 
         // Go to the show view
         $crawler = $client->click($crawler->selectLink('show')->link());
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code");
-    }
+    } */
 
-    */
 }
